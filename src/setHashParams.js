2fa9fbb4f766d56b3a26ffb06d4827a3b8815d87
Vadim Taits <v.taits@netrika.ru>
2017-06-09 15:32:14 +0300

    Removed excess toString from setHashParams

--- src/setHashParams.js
+++ src/setHashParams.js
@@ -5,5 +5,5 @@ export default function setHashParams(params, mapParamsNames, paramsTypes) {
 
   const hashWithoutSearch = window.location.hash.split('?')[0]
 
-  window.location.hash = `${ hashWithoutSearch }?${ paramsStr.toString() }`
+  window.location.hash = `${ hashWithoutSearch }?${ paramsStr }`
 }
