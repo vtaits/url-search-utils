ef90812ab35fc1b57b8f30a5cbd5b45d299f0298
Vadim Taits <taizvadim@gmail.com>
2017-06-09 03:53:22 +0300

    Added tests to stringifyParams with bugfix

--- src/stringifyParams.js
+++ src/stringifyParams.js
@@ -1,13 +1,14 @@
-export default function stringifyParams(params, mapParamsNames, paramsTypes = {}) {
+export default function stringifyParams(params, mapParamsNames = {}, paramsTypes = {}) {
   return Object.keys(params)
     .map((rawParamName) => {
       const paramName = mapParamsNames[rawParamName] || rawParamName
+      const paramValue = params[rawParamName]
 
       if (paramsTypes[paramName]) {
         if (typeof paramsTypes[paramName] === 'function') {
-          const paramValue = paramsTypes[paramName](params[paramName])
+          const mappedParamValue = paramsTypes[paramName](paramValue)
 
-          return `${paramName}=${encodeURIComponent(paramValue)}`
+          return `${paramName}=${encodeURIComponent(mappedParamValue)}`
         }
 
         switch (paramsTypes[paramName]) {
@@ -15,28 +16,28 @@ export default function stringifyParams(params, mapParamsNames, paramsTypes = {}
             return null
 
           case 'include-if-falsy':
-            return `${paramName}=${encodeURIComponent(params[paramName] || '')}`
+            return `${paramName}=${encodeURIComponent(paramValue || '')}`
 
           default:
             throw new Error(`Unknown type of parameter for serialize "${paramName}": "${paramsTypes[paramName]}"`)
         }
       }
 
-      if (params[paramName] instanceof Array) {
-        if (params[paramName].length === 0)  {
+      if (paramValue instanceof Array) {
+        if (paramValue.length === 0)  {
           return null
         }
 
-        return params[paramName]
+        return paramValue
           .map((paramValueItem) => `${paramName}=${encodeURIComponent(paramValueItem)}`)
           .join('&')
       }
 
-      if (!params[paramName]) {
+      if (!paramValue) {
         return null
       }
 
-      return `${paramName}=${encodeURIComponent(params[paramName])}` 
+      return `${paramName}=${encodeURIComponent(paramValue)}` 
     })
     .filter((item) => item !== null)
     .join('&')
